f46c15ac97a1330232c0108b5cc64cab3c3b9463
Ana Julia <anajcaetanom@gmail.com>
2024-10-24 20:09:18 -0300

    inicio

---
+++ integration/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace integration;
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    
+    private static readonly HttpClient client = new HttpClient();
+
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            string url = "http://localhost:3000/alunos";
+            
+            HttpResponseMessage response = await client.GetAsync(url);
+            
+            response.EnsureSuccessStatusCode();
+            
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("erro");
+        }
+    }
+    
+}
